3ea73fea81037b73abc329f4d8281e00ad79724c
joe <homo.technologicus1@gmail.com>
2021-04-17 21:48:26 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by joe_mac on 04/17/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
