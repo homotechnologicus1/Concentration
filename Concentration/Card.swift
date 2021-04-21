3064ad385e7892034409c1745bc59582fe002ef2
joe <homo.technologicus1@gmail.com>
2021-04-21 16:55:19 +0900

    implement basic card functions: flipping, random emoji

---
+++ Concentration/Card.swift
@@ -0,0 +1,25 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by joe_mac on 04/21/2021.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
